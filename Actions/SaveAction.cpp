19c14ba5d9df56148db1c3e746290848db8d2507
unknown <karimemad40@yahoo.com>
2017-05-17 01:51:32 +0400

    RGB

--- Actions/SaveAction.cpp
+++ Actions/SaveAction.cpp
@@ -1,10 +1,7 @@
 #include "SaveAction.h"
-
 #include "..\ApplicationManager.h"
-
 #include "..\GUI\Input.h"
 #include "..\GUI\Output.h"
-
 #include <fstream>
 
 
@@ -29,19 +26,19 @@ bool SaveAction::ReadActionParameters()
 void SaveAction::Execute()
 {
 	ReadActionParameters();
-	if (FileName.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_") != std::string::npos)
+	if (FileName.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_ ") != std::string::npos)
 	{
 		pManager->GetOutput()->PrintMessage("File Name contain invalid characters");
 		return;
 	}
 	ofstream OutFile;
 	OutFile.open("Saved//" + FileName + ".txt", ios::out);
-	OutFile << (int)UI.DrawColor.ucBlue << " " << (int)UI.DrawColor.ucGreen << " " << (int)UI.DrawColor.ucRed
+	OutFile << (int)UI.DrawColor.ucRed << " " << (int)UI.DrawColor.ucGreen << " " << (int)UI.DrawColor.ucBlue
 		<< " " << endl;
-	OutFile << (int)UI.FillColor.ucBlue << " " << (int)UI.FillColor.ucGreen << " " << (int)UI.FillColor.ucRed
+	OutFile << (int)UI.FillColor.ucRed << " " << (int)UI.FillColor.ucGreen << " " << (int)UI.FillColor.ucBlue
 		<< " " << endl;
-	OutFile << (int)UI.BkGrndColor.ucBlue << " " << (int)UI.BkGrndColor.ucGreen 
-		<< " " << (int)UI.BkGrndColor.ucRed<< " " << endl;
+	OutFile << (int)UI.BkGrndColor.ucRed << " " << (int)UI.BkGrndColor.ucGreen
+		<< " " << (int)UI.BkGrndColor.ucBlue << " " << endl;
 	pManager->SaveAll(OutFile);
 	OutFile.close();
 }
